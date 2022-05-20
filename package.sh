bcd8d66abd93e949ca9ec6765bf40190e7a9e97a
Pasi Talvitie <talvitienpasi@gmail.com>
2022-05-20 13:03:58 +0300

    add tag msg

--- package.sh
+++ package.sh
@@ -4,6 +4,6 @@ PREV_VERSION=$(git describe --tags --abbrev=0)
 
 read -p "Previous version is ${PREV_VERSION} please give new version" NEW_VERSION
 
-git tag -a $NEW_VERSION
+git tag -a $NEW_VERSION -m "new version"
 
 FINADDR_BUILD_VERSION=$NEW_VERSION python3 setup.py sdist
